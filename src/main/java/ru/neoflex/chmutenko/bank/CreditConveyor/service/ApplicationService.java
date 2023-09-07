8df62494501e516ebe8b1c0051197dd132f843ab
chmutenkoa <alenachm@gmail.com>
2023-09-07 21:49:35 +0300

    calculations of amount and rate moved to separate methods

--- src/main/java/ru/neoflex/chmutenko/bank/CreditConveyor/service/ApplicationService.java
+++ src/main/java/ru/neoflex/chmutenko/bank/CreditConveyor/service/ApplicationService.java
@@ -51,24 +51,35 @@ public class ApplicationService {
         offer.setIsSalaryClient(isSalaryClient);
         logger.info(String.format("Setting isSalaryClient: %b", isSalaryClient));
 
-        // insurance increases the total amount by 100,000 and reduces the rate by 3 points
-        BigDecimal totalAmount = (isInsuranceEnabled) ? amount.add(insuranceAmount) : amount;
+        BigDecimal totalAmount = calculateTotalAmount(amount, isInsuranceEnabled, insuranceAmount);
         offer.setTotalAmount(totalAmount);
-        logger.info(String.format("Setting totalAmount: %s", totalAmount.toString()));
+        logger.info(String.format("Setting totalAmount: %s", totalAmount));
 
-        BigDecimal totalRate = (isInsuranceEnabled) ? baseRate.subtract(new BigDecimal(3)) : baseRate;
-
-        // salary client reduces the rate by 1 points
-        totalRate = (isSalaryClient) ? totalRate.subtract(new BigDecimal(1)) : totalRate;
+        BigDecimal totalRate = calculateTotalRent(baseRate, isInsuranceEnabled, isSalaryClient);
         offer.setRate(totalRate);
         logger.info(String.format("Setting totalRate: %s", totalRate.toString()));
 
         BigDecimal monthlyPayment = calculateMonthlyPayment(totalAmount, totalRate, term);
         offer.setMonthlyPayment(monthlyPayment);
         logger.info(String.format("Setting monthlyPayment: %s", monthlyPayment));
-        logger.info("The offer created");
 
+        logger.info("The offer created");
         return offer;
+    }
+
+    // insurance increases the total amount by 100,000
+    private BigDecimal calculateTotalAmount(BigDecimal amount, boolean isInsuranceEnabled, BigDecimal insuranceAmount) {
+        return (isInsuranceEnabled) ? amount.add(insuranceAmount) : amount;
+    }
+
+    private BigDecimal calculateTotalRent(BigDecimal baseRate, boolean isInsuranceEnabled, boolean isSalaryClient) {
+        // insurance reduces the rate by 3 points
+        BigDecimal totalRate = (isInsuranceEnabled) ? baseRate.subtract(new BigDecimal(3)) : baseRate;
+
+        // salary client reduces the rate by 1 points
+        totalRate = (isSalaryClient) ? totalRate.subtract(new BigDecimal(1)) : totalRate;
+
+        return totalRate;
     }
 
     private BigDecimal calculateMonthlyPayment(BigDecimal amount, BigDecimal rate, int term) {
