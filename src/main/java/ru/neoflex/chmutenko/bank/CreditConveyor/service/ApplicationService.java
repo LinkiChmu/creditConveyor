8d3f246787555b9f2f549a065a5cc7d1b6565f02
chmutenkoa <alenachm@gmail.com>
2023-09-16 11:27:13 +0300

    logs added to AplicationService

--- src/main/java/ru/neoflex/chmutenko/bank/CreditConveyor/service/ApplicationService.java
+++ src/main/java/ru/neoflex/chmutenko/bank/CreditConveyor/service/ApplicationService.java
@@ -76,13 +76,24 @@ public class ApplicationService {
 
     // formula for calculating annuityRatio = ( monthlyRate * (1 + monthlyRate)^term ) / (1 + monthlyRate)^term - 1
     private BigDecimal calculateMonthlyPayment(BigDecimal amount, BigDecimal rate, int term) {
+        logger.info("Starting calculateMonthlyPayment() with params amount %s, rate %s, term %d"
+                .formatted(amount.toString(), rate.toString(), term));
+
         MathContext mathContext = new MathContext(6, RoundingMode.HALF_UP);
+
         BigDecimal monthlyRate = rate.divide(new BigDecimal(term), mathContext).divide(new BigDecimal(100), mathContext);
+        logger.info("Calculated monthlyRate: %s".formatted(monthlyRate.toString()));
 
         BigDecimal accumulatedRatio = monthlyRate.add(new BigDecimal(1)).pow(term, mathContext);
+        logger.info("Calculated accumulatedRatio: %s".formatted(accumulatedRatio.toString()));
+
         BigDecimal annuityRatio = (monthlyRate.multiply(accumulatedRatio, mathContext))
                 .divide(accumulatedRatio.subtract(new BigDecimal(1)), mathContext);
+        logger.info("Calculated annuityRatio: %s".formatted(annuityRatio.toString()));
 
-        return amount.multiply(annuityRatio, mathContext);
+        BigDecimal monthlyPayment = amount.multiply(annuityRatio, mathContext);
+        logger.info("Calculated monthlyPayment: %s".formatted(monthlyPayment.toString()));
+
+        return monthlyPayment;
     }
 }
